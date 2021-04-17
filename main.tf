57d770b22177da27b9baa78074f7cca5507b858b
Amrit Sandhu <65868030+A-Sandhu@users.noreply.github.com>
2021-04-17 18:41:51 +1200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+#data "local_file" "foo" {
+#    filename = "/tmp/cli.tfrc"
+#}
+#
+#variable "lol" {
+#  description = "this should not be optional"
+#}
+#
+#
+#output "lol" {
+#  value = "${data.local_file.foo.content}"
+#}
+#
+#module "nullresource" {
+#  source  = "localterraform.com/test/nullresource/aws"
+#  version = "0.0.1"
+#}
+#
+#
+#output "example" {
+#  value = "${module.nullresource.example}"
+#}
+
+resource "random_pet" "console_password" {
+    length = 4
+}
+
+output "lol" {
+    value = "${random_pet.console_password.id}"
+}
+
+output "rofl" {
+    value = "${random_pet.console_password.id}"
